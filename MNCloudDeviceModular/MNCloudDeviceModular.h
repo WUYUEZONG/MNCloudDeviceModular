32c10dcbe0dafa60b9227627464ed58f6e2d74aa
CaffeCat <weibin_caffe@outlook.com>
2021-07-21 11:25:53 +0800

    Initial Commit

---
+++ MNCloudDeviceModular/MNCloudDeviceModular.h
@@ -0,0 +1,18 @@
+//
+//  MNCloudDeviceModular.h
+//  MNCloudDeviceModular
+//
+//  Created by 蛮牛科技 on 7/21/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MNCloudDeviceModular.
+FOUNDATION_EXPORT double MNCloudDeviceModularVersionNumber;
+
+//! Project version string for MNCloudDeviceModular.
+FOUNDATION_EXPORT const unsigned char MNCloudDeviceModularVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MNCloudDeviceModular/PublicHeader.h>
+
+
